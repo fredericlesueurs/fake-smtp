357bd8206b276ce0f09876fd5f2d62eecd8dd802
Frédéric Lesueurs <frederic.lesueurs@gmail.com>
2022-08-20 18:07:47 +0200

    Add service for email creation

---
+++ server/src/email/services/email.service.ts
@@ -0,0 +1,95 @@
+import { Injectable } from '@nestjs/common';
+import { AddressObject, ParsedMail } from 'mailparser';
+import { Header } from '../entities/header.entity';
+import { Attachment } from '../entities/attachment.entity';
+import { Email } from '../entities/email.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Recipient, RecipientType } from '../entities/recipient.entity';
+import { Sender } from '../entities/sender.entity';
+
+@Injectable()
+export class EmailService {
+  constructor(
+    @InjectRepository(Email) private emailRepository: Repository<Email>,
+  ) {}
+
+  async createEmail(parsedMail: ParsedMail): Promise<void> {
+    let recipientsCc = [];
+    let recipientsBcc = [];
+
+    const headers = parsedMail.headerLines.map((headerLine): Header => {
+      return Header.create(headerLine.key, headerLine.line);
+    });
+
+    const attachments = parsedMail.attachments.map((attachment): Attachment => {
+      return Attachment.create(
+        attachment.type,
+        attachment.contentType,
+        attachment.contentDisposition,
+        attachment.filename,
+        attachment.checksum,
+        attachment.size,
+      );
+    });
+
+    const recipientsTo = this.extractRecipients(parsedMail.to);
+
+    if (parsedMail.cc) {
+      recipientsCc = this.extractRecipients(parsedMail.cc);
+    }
+
+    if (parsedMail.bcc) {
+      recipientsBcc = this.extractRecipients(parsedMail.bcc);
+    }
+
+    const senders = this.extractSenders(parsedMail.from);
+
+    const email = new Email();
+    email.senders = senders;
+    email.attachments = attachments;
+    email.recipients = [...recipientsTo, ...recipientsBcc, ...recipientsCc];
+    email.headers = headers;
+    email.date = parsedMail.date;
+    email.html = parsedMail.html ? parsedMail.html : null;
+    email.text = parsedMail.text;
+    email.messageId = parsedMail.messageId;
+    email.subject = parsedMail.subject;
+
+    console.log(email);
+
+    await this.emailRepository.save(email);
+  }
+
+  private extractRecipients(
+    parsedMailRecipients: AddressObject | AddressObject[],
+  ): Recipient[] {
+    if (Array.isArray(parsedMailRecipients)) {
+      return parsedMailRecipients
+        .map((recipient) => {
+          return recipient.value.map((recipient) => {
+            return Recipient.create(
+              recipient.name,
+              recipient.address,
+              RecipientType.TO,
+            );
+          });
+        })
+        .flat();
+    }
+
+    return parsedMailRecipients.value.map((recipient) => {
+      return Recipient.create(
+        recipient.name,
+        recipient.address,
+        RecipientType.TO,
+      );
+    });
+  }
+
+  private extractSenders(parsedMailSender: AddressObject): Sender[] {
+    return parsedMailSender.value.map((sender) => {
+      return Sender.create(sender.name, sender.address);
+    });
+  }
+}
